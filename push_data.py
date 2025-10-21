266d38226ce4cb2027d8637f82ecd70df58286d7
garvkhullar1121 <garvkhullar1121@gmail.com>
2025-10-21 08:23:27 +0530

    mongodb connections and pushing

---
+++ push_data.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import json
+from dotenv import load_dotenv
+import certifi
+import pandas as pd
+import numpy as np
+import pymongo
+from network_security.exception.exception import NetworkSecurityException
+from network_security.logging.logger import logging
+
+load_dotenv()
+
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+print("MongoDB URL:", MONGO_DB_URL)
+
+ca = certifi.where()
+
+class NetworkDataExtract:
+
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    
+    def csv_to_json_converter(self, file_path):
+        try:
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True, inplace=True)
+            records = list(json.loads((data.T.to_json())).values())
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    
+    def insert_data_mongo(self, records, database, collection):
+        try:
+            mongo_client = pymongo.MongoClient(MONGO_DB_URL, tlsCAFile=ca)
+            db = mongo_client[database]
+            col = db[collection]
+            col.insert_many(records)
+            return len(records)
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+
+if __name__ == "__main__":
+    FILE_PATH = r"Network_data\phisingData.csv"
+    DATABASE = "Garv"
+    COLLECTION = "NetworkData"
+    
+    network_obj = NetworkDataExtract()
+    records = network_obj.csv_to_json_converter(file_path=FILE_PATH)
+    print("Sample Record:", records[:1])
+    
+    no_of_records = network_obj.insert_data_mongo(records, DATABASE, COLLECTION)
+    print(f"Inserted {no_of_records} records successfully.")
---
+++ test_mongo_db.py
@@ -0,0 +1,14 @@
+
+from pymongo import MongoClient
+
+uri = "mongodb+srv://garvkhullar1121_db_user:Garv123@cluster0.edswblb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
